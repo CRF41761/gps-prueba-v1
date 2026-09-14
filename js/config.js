0b04a687e8e3f46e6030562a2218f1acf1470d24
CRF41761 <crf.granja.saler@gmail.com>
2026-09-14 11:16:26 +0200

    Add configuration settings in config.js
    Initial configuration setup for the application.

---
+++ js/config.js
@@ -0,0 +1,36 @@
+/* =========================================================
+   CONFIGURACIÓN
+========================================================= */
+
+const CONFIG = {
+
+    modo: "mock",
+
+    mapa: {
+        centro: [39.4699, -0.3763],
+        zoom: 9
+    },
+
+    api: {
+
+        url:
+            "https://script.google.com/macros/s/AKfycbz9cowY8r7adCxP5eFFMCH9yaXTl1a5g5VpETQBXJuS_IFas--YpWgquuKBk4oiL9qw/exec"
+    },
+
+    refrescoGPS: 5000,
+
+    coloresRutas: {
+
+        R1: "#1976D2",
+        R2: "#388E3C",
+        R3: "#F57C00"
+    },
+
+    nombresRutas: {
+
+        R1: "Norte",
+        R2: "Interior",
+        R3: "Sur"
+    }
+
+};
